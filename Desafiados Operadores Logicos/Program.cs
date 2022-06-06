c9ce63e5838fef6352413b1e625d263e2fb9b7d3
João Victor Caires <78885438+srkayrez@users.noreply.github.com>
2022-06-06 10:54:28 -0300

    Add files via upload

---
+++ Desafiados Operadores Logicos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int qtdNotas;
+        double notaFinal = 0, media;
+        Console.WriteLine("Digite quantas notas o aulo teve: ");
+        qtdNotas = int.Parse(Console.ReadLine());
+
+        double[] notas = new double[qtdNotas];
+
+        for (int i = 0; i < qtdNotas; i++)
+        {
+            Console.WriteLine($"Digite a {i+1} nota: ");
+            notas[i] = int.Parse(Console.ReadLine());
+
+            notaFinal += notas[i];
+        }
+        
+        media = notaFinal / qtdNotas;
+
+        if (media >= 7)
+        {
+            Console.WriteLine($"Aluno aprovado com {media}");
+        }
+        else if (media <= 5)
+        {
+            Console.WriteLine($"Aluno reprovado com {media}");
+        }
+        else if (media >= 5 && media <= 7)
+        {
+            Console.WriteLine($"Alno em recuperação com {media}");
+        }
+    }
+}
---
+++ Exercicio ternario/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int qtdNotas;
+        double notaFinal = 0, media;
+        Console.WriteLine("Digite quantas notas o aulo teve: ");
+        qtdNotas = int.Parse(Console.ReadLine());
+
+        double[] notas = new double[qtdNotas];
+
+        for (int i = 0; i < qtdNotas; i++)
+        {
+            Console.WriteLine($"Digite a {i + 1} nota: ");
+            notas[i] = int.Parse(Console.ReadLine());
+
+            notaFinal += notas[i];
+
+        }
+
+        media = notaFinal / qtdNotas;
+
+        var situacao = (media >= 7 ) ? "aprovado" : "reprovado";
+       
+        Console.WriteLine(situacao);
+    }
+}
